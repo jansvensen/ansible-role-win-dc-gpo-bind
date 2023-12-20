36c87390eba07fb8ba3a66ebb901b51c3ae2c98c
Sven Jansen <github@jansvensen.de>
2023-12-20 12:32:20 +0100

    .

--- files/gpo-bind.ps1
+++ files/gpo-bind.ps1
@@ -1,6 +1,7 @@
 $target = $env:target
 $gpos = $env:gpos
+$domain = $env:domain
 
 $gpos | ForEach-Object {
-    New-GPLink -Name $_ -Domain dt.democloud -Target $target -LinkEnabled Yes
+    New-GPLink -Name $_ -Domain $domain -Target $target -LinkEnabled Yes
 }
