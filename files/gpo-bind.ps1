e372337f4f8f763151ded150e9710973940e8f0f
Sven Jansen <github@jansvensen.de>
2023-12-20 12:36:00 +0100

    .

--- files/gpo-bind.ps1
+++ files/gpo-bind.ps1
@@ -2,6 +2,6 @@ $target = $env:target
 $gpos = $env:gpos
 $domain = $env:domain
 
-$gpos | ForEach-Object {
+$gpos.Split(",") | ForEach-Object {
     New-GPLink -Name $_ -Domain $domain -Target $target -LinkEnabled Yes
 }
