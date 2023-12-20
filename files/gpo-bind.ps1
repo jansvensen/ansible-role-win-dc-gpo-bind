2b0dd80eb89237dba30e8d428edb7e1349b54672
Sven Jansen <github@jansvensen.de>
2023-12-20 12:16:37 +0100

    .

--- files/gpo-bind.ps1
+++ files/gpo-bind.ps1
@@ -8,10 +8,4 @@ function link-gpos {
     }
 }
 
-$target = "OU=Objects,DC=dt,DC=democloud"
-$GPOs = "GPO_C_IPv6","GPO_C_PowerSettings","GPO_U_Optics_Wallpaper","GPO_U_Optics_User"
-link-gpos -target $target -GPOs $GPOs
-
-$target = "OU=Computer,OU=Objects,DC=dt,DC=democloud"
-$GPOs = "GPO_C_DisableServerManagerOnStartup","GPO_C_LoopbackProcessing","GPO_C_NewsAndInterests","GPO_C_deviceTRUST_Geolocation_Google","GPO_C_RegionSettings","GPO_C_deviceTRUST_Basic","GPO_C_deviceTRUST_Demo_Demobox"
 link-gpos -target $target -GPOs $GPOs
